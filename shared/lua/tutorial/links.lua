112d2b116602903d1995d82ae6c8b8e189a5b733
CapsAdmin <eliashogstvedt@gmail.com>
2016-09-13 18:38:00 +0200

    update links

--- shared/lua/tutorial/links.lua
+++ shared/lua/tutorial/links.lua
@@ -1,6 +1,9 @@
 --[[
 	[luacraft wiki](macro:inline(wx.wxLaunchDefaultBrowser("https://wiki.luacraft.com/LuaCraft")))
 	[luacraft source code on github](macro:inline(wx.wxLaunchDefaultBrowser("https://github.com/luastoned/LuaCraft/")))
-	[.minecraft folder](macro:inline(ShowLocation("./../../" .. jit.os:lower() .. "/minecraft/run")))
+	[block ids](macro:inline(wx.wxLaunchDefaultBrowser("http://minecraft-ids.grahamedgecombe.com/")))
+	[entity list](macro:inline(wx.wxLaunchDefaultBrowser("http://minecraft-ids.grahamedgecombe.com/entities")))
+	[server .minecraft folder](macro:inline(ShowLocation("./../../" .. jit.os:lower() .. "/minecraft/run_server")))
+	[client .minecraft folder](macro:inline(ShowLocation("./../../" .. jit.os:lower() .. "/minecraft/run_client")))
 	[luacraft java source code](macro:inline(ShowLocation("./../../" .. jit.os:lower() .. "/minecraft/src/main/java/com/luacraft/")))
 ]]
