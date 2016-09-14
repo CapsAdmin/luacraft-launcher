0a1317d59eb7c7ebbdc90674860f5491ce5f77e1
CapsAdmin <eliashogstvedt@gmail.com>
2016-09-14 12:08:22 +0200

    disable some style features

--- shared/ide/config.lua
+++ shared/ide/config.lua
@@ -1,12 +1,13 @@
-local G = ...
-
 editor.usetabs = true
 editor.tabwidth = 4
 editor.usewrap = false
 editor.fontsize = 9
 editor.menuicon = true
 format.apptitle = "Luacraft IDE"
+styles.indicator.varlocal = nil
 styles.indicator.varglobal = nil
+styles.indicator.varmasking = nil
+styles.indicator.varmasked = nil
 
 toolbar.icons = {}
 
